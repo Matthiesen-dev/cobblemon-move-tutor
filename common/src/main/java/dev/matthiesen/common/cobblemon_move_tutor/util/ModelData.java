92c3764e92ae865ad4e3e587c597a240c461fc75
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-10 18:28:38 -0700

    Update custom model data values in GUI configuration and ModelData class

--- common/src/main/java/dev/matthiesen/common/cobblemon_move_tutor/util/ModelData.java
+++ common/src/main/java/dev/matthiesen/common/cobblemon_move_tutor/util/ModelData.java
@@ -2,16 +2,16 @@ package dev.matthiesen.common.cobblemon_move_tutor.util;
 
 public class ModelData {
     public static class GUI_BUTTON {
-        public static final int GUI_CONFIRM = 1;
-        public static final int GUI_CANCEL = 2;
-        public static final int GUI_PREVIOUS = 6;
-        public static final int GUI_NEXT = 7;
-        public static final int GUI_PAGE = 8;
+        public static final int GUI_CONFIRM = 1001;
+        public static final int GUI_CANCEL = 1002;
+        public static final int GUI_PREVIOUS = 1003;
+        public static final int GUI_NEXT = 1004;
+        public static final int GUI_PAGE = 1005;
     }
 
     public static class GUI_TEXT {
-        public static final int CHOOSE_POKEMON = 3;
-        public static final int CONFIRM_TEACH = 4;
-        public static final int SELECT_MOVE = 5;
+        public static final int CHOOSE_POKEMON = 2001;
+        public static final int CONFIRM_TEACH = 2002;
+        public static final int SELECT_MOVE = 2003;
     }
 }
