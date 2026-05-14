d8838beb5e537cad4dc60cb7c430098f442cd4e2
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-13 20:48:22 -0700

    Refactor ModPermission to extend AbstractPermission and simplify implementation

--- common/src/main/java/dev/matthiesen/common/cobblemon_move_tutor/permissions/ModPermission.java
+++ common/src/main/java/dev/matthiesen/common/cobblemon_move_tutor/permissions/ModPermission.java
@@ -1,61 +1,21 @@
 package dev.matthiesen.common.cobblemon_move_tutor.permissions;
 
 import dev.matthiesen.common.cobblemon_move_tutor.Constants;
-import dev.matthiesen.common.matthiesen_lib.interfaces.Permission;
+import dev.matthiesen.common.matthiesen_lib.permission.AbstractPermission;
 import dev.matthiesen.common.matthiesen_lib.permission.PermissionLevel;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.NotNull;
 
-public class ModPermission implements Permission {
-    private final String node;
-    private final PermissionLevel level;
-    private final ResourceLocation identifier;
-    private final String literal;
+public class ModPermission extends AbstractPermission {
+    @Override
+    protected String getModId() {
+        return Constants.MOD_ID;
+    }
+
+    @Override
+    protected String getPermissionNamespace() {
+        return "CobblemonPokeTotem";
+    }
 
     public ModPermission(String node, PermissionLevel level) {
-        this.node = node;
-        this.level = level;
-        this.identifier = Constants.modResource(this.node);
-        this.literal = Constants.MOD_ID + "." + this.node;
-    }
-
-    @Override
-    public @NotNull ResourceLocation getIdentifier() {
-        return identifier;
-    }
-
-    @Override
-    public @NotNull String getLiteral() {
-        return literal;
-    }
-
-    @Override
-    public @NotNull PermissionLevel getLevel() {
-        return level;
-    }
-
-    // Optional: equals, hashCode, and toString methods for data class behavior
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ModPermission that = (ModPermission) o;
-        return java.util.Objects.equals(node, that.node) &&
-                java.util.Objects.equals(level, that.level);
-    }
-
-    @Override
-    public int hashCode() {
-        return java.util.Objects.hash(node, level);
-    }
-
-    @Override
-    public String toString() {
-        return "CobblemonPokeTotem{" +
-                "node='" + node + '\'' +
-                ", level=" + level +
-                ", identifier=" + identifier +
-                ", literal='" + literal + '\'' +
-                '}';
+        super(node, level);
     }
 }
